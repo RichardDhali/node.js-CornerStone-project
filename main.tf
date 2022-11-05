bf9a1a3a4d7e3812f495393c2791ce556f0cc378
ShubbamShrmali <111654213+ShubbamShrmali@users.noreply.github.com>
2022-11-04 23:53:45 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,30 @@
+# terraform {
+#     required_version = "1.3.4"
+#     required_providers {
+#         aws = {
+#             source = "hashicorp/aws"
+#             version = "4.16"
+#         }
+#     }
+# }
+
+ provider "aws" {
+   region  = "us-east-1"
+}
+
+
+
+
+resource "aws_instance" "ELKDeploy" {
+    ami = "ami-026b57f3c383c2eec"
+    count = "1"
+    key_name   = "Jenkins"
+    instance_type = "t3.large"
+    security_groups = ["launch-wizard-1"]
+    tags = {
+        Name = "ELK-Stack-deploy"
+    } 
+    user_data = file("elk.sh")
+   
+
+}
