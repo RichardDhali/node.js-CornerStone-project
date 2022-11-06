d86d41e3ed146073203fe17d773145f8b53d32e7
Richard Dhali <60823958+RichardDhali@users.noreply.github.com>
2022-11-06 15:43:21 -0600

    Update node.sh

--- node.sh
+++ node.sh
@@ -8,8 +8,8 @@ chkconfig docker on
 yum install -y git
 curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
-git clone https://github.com/ShubbamShrmali/node.js-CornerStone.git
-cd /node.js-CornerStone/
-docker build -t noode .
-docker run -t -d -p 3030:3030 noode
+git clonehttps://github.com/RichardDhali/node.js-CornerStone-project.git
+cd /node.js-CornerStone-project/
+docker build -t shitlink .
+docker run -t -d -p 3030:3030 shitlink
 
